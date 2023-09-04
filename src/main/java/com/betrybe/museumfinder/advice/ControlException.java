d5a9df5f07647f2a65deafc612e1d3bc94c17db6
BrunoSouza <desouza88@hotmail.com>
2023-09-04 16:40:49 -0300

    feat: req07

---
+++ src/main/java/com/betrybe/museumfinder/advice/ControlException.java
@@ -0,0 +1,31 @@
+package com.betrybe.museumfinder.advice;
+
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * javadoc.
+ */
+@ControllerAdvice
+public class ControlException {
+
+  @ExceptionHandler(InvalidCoordinateException.class)
+  public ResponseEntity<String> handleInvalidCoordinateException(
+          InvalidCoordinateException exception) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coordenada inválida!");
+  }
+
+  @ExceptionHandler(MuseumNotFoundException.class)
+  public ResponseEntity<String> handleMuseumNotFoundException(MuseumNotFoundException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Museu não encontrado!");
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception exception) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno!");
+  }
+}
